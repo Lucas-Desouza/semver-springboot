5128ea4714ac4171a500d3d69572a93623b23849
Lucas Desouza <lucas.desouza@aa.com>
2022-08-30 10:32:59 -0500

    init

---
+++ src/main/java/com/example/semverdemo/SemverDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.semverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemverDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemverDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/semverdemo/SemverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemverDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
